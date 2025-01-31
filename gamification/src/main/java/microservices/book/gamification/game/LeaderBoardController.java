d06145ae6e26e7fd7047a4fb215afdb8d074dac3
ArtomDovgal <artom.dovgalnew@gmail.com>
2025-01-31 23:58:02 +0200

    First commit

---
+++ gamification/src/main/java/microservices/book/gamification/game/LeaderBoardController.java
@@ -0,0 +1,28 @@
+package microservices.book.gamification.game;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import microservices.book.gamification.game.domain.LeaderBoardRow;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+/**
+ * This class implements a REST API for the Gamification LeaderBoard service.
+ */
+@Slf4j
+@RestController
+@RequestMapping("/leaders")
+@RequiredArgsConstructor
+class LeaderBoardController {
+
+    private final LeaderBoardService leaderBoardService;
+
+    @GetMapping
+    public List<LeaderBoardRow> getLeaderBoard() {
+        log.info("Retrieving leaderboard");
+        return leaderBoardService.getCurrentLeaderBoard();
+    }
+}
